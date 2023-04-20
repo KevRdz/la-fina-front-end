6c625a4697e61fb6a751c579344ca45cb415fb31
Kevin <kevin.rodriguez678@gmail.com>
2023-04-20 12:14:31 -0400

    remove landing page, fix links and add styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,25 @@
 import { Link } from 'react-router-dom'
+// import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
-      {user ?
+      <header className='App-header'>
+        La Fina Postres
+        {user ?
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/profiles">Profiles</Link>
+            <Link to="" onClick={handleLogout}>LOG OUT</Link>
+            <Link to="/changePassword">Change Password</Link>
+          </nav>
+        :
         <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
-        </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
+            <Link to="/login">Log In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </nav>
+        }
+      </header>
     </>
   )
 }
